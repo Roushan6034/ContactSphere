a8cf3ccc4e8ebe80d419ccd5d639a4c287ee5a82
Roushan <sidharth6034@gmail.com>
2026-03-02 23:43:40 +0530

    Security configuration

--- src/main/java/org/majorproject/contactsphere/ContactSphereApplication.java
+++ src/main/java/org/majorproject/contactsphere/ContactSphereApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ContactSphereApplication {
 
     public static void main(String[] args) {
+        System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
         SpringApplication.run(ContactSphereApplication.class, args);
     }
 
